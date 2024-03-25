e7a930b29ac05c929635a80611abdf6803879d1d
Roman Annenkov <roman.annenkov@gmail.com>
2024-03-25 12:53:00 +0300

    ImagePixelColorsToElementColors converter optimized

--- src/main/java/model/converter/ImagePixelColorsToElementColors.java
+++ src/main/java/model/converter/ImagePixelColorsToElementColors.java
@@ -4,6 +4,9 @@ import model.image.Image;
 import model.elementColorsLoader.IMosaicElementColorLoader;
 import model.color.Color;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ImagePixelColorsToElementColors implements IImagePixelColorsToElementColors {
     IPixelColorToElementColor pixelColorToElementColor;
     IMosaicElementColorLoader mosaicElementColorLoader;
@@ -17,10 +20,18 @@ public class ImagePixelColorsToElementColors implements IImagePixelColorsToEleme
     @Override
     public void convertPixelColorsToElementColors(Image image) {
 
+        Map<Color, Color> foundColorRelations = new HashMap<>();
+
         for (int i = 0; i < image.getHeight(); i++) {
             for (int j = 0; j < image.getWidth(); j++) {
-                Color convertedColor = pixelColorToElementColor.getClosestElementColor(image.getDotColor(i, j), mosaicElementColorLoader);
-                image.setDotColor(i, j, convertedColor);
+                Color currentColor = image.getDotColor(i, j);
+                if (foundColorRelations.containsKey(currentColor)) {
+                    image.setDotColor(i, j, foundColorRelations.get(currentColor));
+                } else {
+                    Color convertedColor = pixelColorToElementColor.getClosestElementColor(currentColor, mosaicElementColorLoader);
+                    foundColorRelations.put(currentColor, convertedColor);
+                    image.setDotColor(i, j, convertedColor);
+                }
             }
         }
     }
